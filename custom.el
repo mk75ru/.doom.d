4dac43e1e491c327dbf470eae00618f12cf5e2f2
Mikhail K <mk75ru@gmail.com>
2022-06-24 23:43:30 +0700

    увеличил размер шрифта

--- custom.el
+++ custom.el
@@ -3,12 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (wgrep))))
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages '(wgrep))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Droid Sans Mono Slashed" :foundry "1ASC" :slant normal :weight normal :height 158 :width normal)))))
 (put 'customize-group 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
