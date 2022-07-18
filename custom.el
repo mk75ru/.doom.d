697d0abe1237d5bf79b7b88f98da0671d844cb91
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2022-07-18 17:13:57 +0700

    подправил цвета в редакторе C++

--- custom.el
+++ custom.el
@@ -3,12 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (wgrep))))
+ '(package-selected-packages '(wgrep)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-builtin-face ((t (:foreground "magenta3"))))
+ '(font-lock-comment-face ((t (:foreground "saddle brown"))))
+ '(font-lock-constant-face ((t (:foreground "orange red"))))
+ '(font-lock-function-name-face ((t (:foreground "royal blue"))))
+ '(font-lock-type-face ((t (:foreground "magenta1"))))
+ '(font-lock-variable-name-face ((t (:foreground "orange")))))
 (put 'customize-group 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
