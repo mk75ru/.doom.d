1c1d6cd331a4b860813c8c22abf895854d3fcffd
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2022-07-25 09:56:40 +0700

    вынес настройки шрифтов в doom-custom.d/custom.el

--- custom.el
+++ custom.el
@@ -14,13 +14,16 @@
  '(woman-manpath
    '("/usr/share/gcc-data/x86_64-pc-linux-gnu/11.3.0/man" "/usr/share/binutils-data/x86_64-pc-linux-gnu/2.37_p1/man" "/usr/local/share/man" "/usr/share/man" "/usr/lib/llvm/14/share/man" "~/.cache/cppman")))
 
- '(package-selected-packages '(wgrep))
+'(package-selected-packages '(wgrep))
+
+(add-load-path!  "doom-custom.d")
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Droid Sans Mono Slashed" :foundry "1ASC" :slant normal :weight normal :height 100 :width normal))))
+
+ '(setfont)
  '(font-lock-builtin-face ((t (:foreground "magenta3"))))
  '(font-lock-comment-face ((t (:foreground "saddle brown"))))
  '(font-lock-constant-face ((t (:foreground "orange red"))))
